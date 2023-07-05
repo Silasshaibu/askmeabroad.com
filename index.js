f8784ee6b2cb70ece8962245a45a21693b7e2e62
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-07-05 01:49:23 +0100

    Commit

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ whatsappChatIcon.addEventListener('click', () => {
     whatsappPopUpChatBox.style.display ='grid';
   });
 
-
+//Remove whtasappPopUpChatBox when user clicks on whatsappChatIcon
 whatsappCloseBtn.addEventListener('click', () => {
     whatsappPopUpChatBox.style.display ='none';
   });
+
+  const body = document.getElementById('body');
+  const mobileMenuButton = document.getElementById('mobileNavCloseBtn');
+  const mobileMenuDropDownNav = document.getElementById('mobilemenuDropDownNav');
+  //Show mobileNavDropdown when user clicks on this icon
+  mobileMenuButton.addEventListener('click', () => {
+  mobileMenuDropDownNav.style.display ='block';
+  console.log('ShowMobileMenuDropDown');
+  body.style.transform = 'translateX(0%)';
+});
