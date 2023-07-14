0ae090cfe509dea19ee49d27462fecb96a35dac7
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-07-15 00:46:26 +0100

    Commit

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 const whatsappChatIcon = document.getElementById('whatsappIcon');
+
 const whatsappPopUpChatBox = document.getElementById('whatsappchatBoxPopUp');
-const whatsappCloseBtn = document.getElementById('whatsappCloseBtn');
 
-//Show whtasappPopUpChatBox when user clicks on whatsappChatIcon
-whatsappChatIcon.addEventListener('click', () => {
-    whatsappPopUpChatBox.style.display ='grid';
-  });
+//Opening the whatsappPopUpChat with the whatsapp Close btn
+function openWhatsappPopUp(){
+  whatsappPopUpChatBox.style.display ='grid';
+}
 
-//Remove whtasappPopUpChatBox when user clicks on whatsappChatIcon
-whatsappCloseBtn.addEventListener('click', () => {
-    whatsappPopUpChatBox.style.display ='none';
-  });
-
-  const body = document.getElementById('body');
-  const mobileMenuButton = document.getElementById('mobileNavCloseBtn');
-  const mobileMenuDropDownNav = document.getElementById('mobilemenuDropDownNav');
-  //Show mobileNavDropdown when user clicks on this icon
-  mobileMenuButton.addEventListener('click', () => {
-  mobileMenuDropDownNav.style.display ='block';
-  console.log('ShowMobileMenuDropDown');
-  body.style.transform = 'translateX(0%)';
-  console.log('bodyToTranslateX-axis__unit')
-});
+//Closing the whatsappPopUpChat with the whatsapp Close btn
+function whatsappCloseBtn(){
+  whatsappPopUpChatBox.style.display ='none';
+}
 
 
 //Change the color of the whatsapp Contact Icon
@@ -50,6 +39,14 @@ setInterval(changeBackgroundColor, .1 * 30 * 1000);
 //on click on close button
 
 
+const body = document.getElementById('body');
+const mobileMenuButton = document.getElementById('mobileNavCloseBtn');
+const mobileMenuDropDownNav = document.getElementById('mobilemenuDropDownNav');
+
+function mobileMenuOpen(){
+mobileMenuDropDownNav.style.display ='block';
+body.style.transform = 'translateX(0%)';
+}
 
 
 
