64aee40019c34f8613813bda4a09fc4acd08b9f4
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-07-22 13:55:35 +0100

    Commit

---
+++ askExpert.js
@@ -0,0 +1,10 @@
+//CallTheFooterFrom index.js
+
+// function addHeader(){
+//     const footerComponent =  document.getElementById('footerComponent');
+//     const sectionFooter = document.querySelector('componentFooter');
+//     sectionFooter.appendChild(footerComponent);
+//     console.log(sectionFooter);
+// }
+
+// addHeader();
