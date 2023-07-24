bbbe9ec892ad3757a99bcbb5a446216b700624a0
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-07-24 08:48:07 +0100

    Commit

--- footer-component.js
+++ footer-component.js
@@ -2,7 +2,136 @@ class FooterComponent extends HTMLElement{
     constructor(){
         super();
         this.innerHTML=`
+        <footer>
+        <div class="single section secondaryAdBoard">
+            <div>
+                <h1>Are You Looking For Study <span class="accent">Abroad Visa Just Call Us</span></h1>
+                <p>Need A Study Abroad Visa Consultation? Call us today <span class="accent">+91 8053064652</span> or
+                    email us : help@askmeabroad.com
+                </p>
+            </div>
+            <div>
+                <button class="primary">Contact Us</button>
+            </div>
 
+        </div>
+        <div class="foot-layer1">
+            <div>
+                <ul>Our Policy
+                    <li><a href="./privacy.html">Privacy Policy</a></li>
+                    <li><a href="./terms-conditions.html">Terms & Conditions</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>Quick Links
+                    <li><a href="./home.html">Home</a></li>
+                    <li><a href="./PTE-Training.html">Pte Prep</a></li>
+                    <li><a href="./how-it-works.html">How it works</a></li>
+                    <li><a href="./blog.html">Blog</a></li>
+                    <li><a href="./contact-us.html">Contact Us</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>Our Location
+                    <li><a href="./IELTS_Coaching_in_Karnal.html">IELTS Coaching in Karnal</a></li>
+                    <li><a href="./PTE_Coaching_in_Karnal.html">PTE Coaching in Karnal</a></li>
+                    <li><a href="./Visa_Consultant_in_Karnal.html">Visa Consultant in Karnal</a></li>
+                    <li><a href="./Visa_Consultancy_in_Panipat.html">Visa Consultancy in Panipat</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>Pricing
+                    <li><a href="./PTE-Online-Class-Package.html">Ielts/Pte Training</a></li>
+                    <li><a href="./University-Enrollment-Package.html">University Enrollment</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="foot-layer2">
+            <div class="socialLinks-with-copyrights">
+                <ul>
+                    <li>
+                        <svg id="Facebook" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64.89 121.3">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: #fff;
+                                        fill-rule: evenodd;
+                                    }
+                                </style>
+                            </defs>
+                            <path class="cls-1"
+                                d="M62.32,135.65h23.5V81.44h17.35l3.47-21.9H85.82V43c0-6.14,5.87-9.61,11.48-9.61h10.15V15.21l-18.16-.8c-17.36-1.07-27,12.55-27,28.84V59.54H42.55v21.9H62.32Z"
+                                transform="translate(-42.55 -14.35)" />
+                        </svg>
+                    </li>
+                    <li>
+                        <svg id="Instagram" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 131.29 130.98">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: #fff;
+                                        fill-rule: evenodd;
+                                    }
+                                </style>
+                            </defs>
+                            <path class="cls-1"
+                                d="M44.76,9.51h60.48a35.57,35.57,0,0,1,35.4,35.41v60.16a35.57,35.57,0,0,1-35.4,35.41H44.76a35.57,35.57,0,0,1-35.4-35.41V44.92A35.57,35.57,0,0,1,44.76,9.51Zm65.49,22.88h0a7.62,7.62,0,0,1,7.52,7.52h0a7.63,7.63,0,0,1-7.52,7.52h0a7.42,7.42,0,0,1-7.52-7.52h0a7.42,7.42,0,0,1,7.52-7.52ZM74.84,39.28h.32A36.11,36.11,0,0,1,110.88,75h0a35.91,35.91,0,0,1-35.72,35.72h-.32A35.64,35.64,0,0,1,39.44,75h0c0-19.43,16-35.72,35.4-35.72Zm0,12.22h.32A23.67,23.67,0,0,1,98.66,75h0a23.74,23.74,0,0,1-23.5,23.81h-.32A23.74,23.74,0,0,1,51.34,75h0a23.67,23.67,0,0,1,23.5-23.5ZM45.08,20.79h59.84a24.47,24.47,0,0,1,24.44,24.44v59.54a24.47,24.47,0,0,1-24.44,24.44H45.08a24.47,24.47,0,0,1-24.44-24.44V45.23A24.47,24.47,0,0,1,45.08,20.79Z"
+                                transform="translate(-9.36 -9.51)" />
+                        </svg>
+                    </li>
+                    <li>
+                        <svg id="Telegram" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 131.58 109.17">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: #fff;
+                                        fill-rule: evenodd;
+                                    }
+                                </style>
+                            </defs>
+                            <path class="cls-1"
+                                d="M17.25,71.86c29.86-12.52,66-30.34,97.28-42.38,28.9-10.6,28.42-7.71,24.08,21.19-3.37,23.11-7.22,46.23-11.07,69.35-1.45,11.07-7.23,16.85-19.27,10.11L70.71,103.64c-5.3-4.33-3.85-7.7.48-12l34.67-32.74c9.15-9.15,4.82-12.53-5.77-5.3L52.41,85.82c-6.74,4.82-14,4.82-21.67,2.41L14.36,82.45C3.77,78.6,12,74.27,17.25,71.86Z"
+                                transform="translate(-9.21 -23.84)" />
+                        </svg>
+                    </li>
+                    <li>
+                        <svg id="Twitter150x150" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 95.86 77.86">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: #fff;
+                                        fill-rule: evenodd;
+                                    }
+                                </style>
+                            </defs>
+                            <g id="Twitter">
+                                <path class="cls-1"
+                                    d="M75.86,63.65C72,43.57,96.2,31.61,110.44,46.11a50.82,50.82,0,0,0,12-5.34A22,22,0,0,1,113.49,52s7.63-1,10.93-3.05c0,0-3.3,6.86-9.4,9.4,3.3,46-46.28,74-86.46,51.12,0,0,19.83,1,28.22-8.65,0,0-12.2,1-18.3-13.73a12.25,12.25,0,0,0,9.15-.51S33.39,84.25,32.12,67.21c0,0,4.58,3.56,9.66,2.29,0,0-15.76-9.41-6.86-26.44,0,0,16.78,21.36,40.94,20.59Z"
+                                    transform="translate(-28.56 -39.71)" />
+                            </g>
+                        </svg>
+                    </li>
+                    <li>
+                        <svg id="Youtube" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 116.96 82.83">
+                            <defs>
+                                <style>
+                                    .cls-1 {
+                                        fill: #fff;
+                                        fill-rule: evenodd;
+                                    }
+                                </style>
+                            </defs>
+                            <path class="cls-1"
+                                d="M132.11,51.79C130.29,38.36,123.47,36.09,110,35c-11.6-.91-24.8-1.36-35-1.36S51.33,34,40,35c-13.43,1.14-20.48,3.41-22.3,16.84A223.07,223.07,0,0,0,16.52,75a219.86,219.86,0,0,0,1.14,23.21c1.82,13.65,8.87,15.7,22.3,16.84,11.37,1.14,24.8,1.36,35,1.36s23.44-.22,35-1.36c13.43-1.14,20.25-3.19,22.07-16.84A167.47,167.47,0,0,0,133.48,75,169.72,169.72,0,0,0,132.11,51.79Zm-68.94,41V57.48L93.66,75Z"
+                                transform="translate(-16.52 -33.59)" />
+                        </svg>
+                    </li>
+                </ul>
+                <p>All Right Reserved. Copyright ©<span id="currentYear"></span> Askmeabroad.</p>
+            </div>
+        </div>
+    </footer>
 
         <section class="ActionBodyBottom">
         <div class="whatsappContainer-All">
