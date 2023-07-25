467ab87a3886f17fc1934dce7a894a8e73cba2c7
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-07-25 11:07:02 +0100

    Commit

--- header-component.js
+++ header-component.js
@@ -150,14 +150,14 @@ class HeaderComponent extends HTMLElement{
     `;
     }
 
-    // Method to update the veryFirstText content
-     updateHeaderText(newText) {
-        const heroFirstText = this.querySelector('#heroFirstText');
-        if (heroFirstText) {
-            console.log(`Here we are`);
-            heroFirstText.innerHTML = newText;
-        }
-    }
+    // // Method to update the veryFirstText content
+    //  updateHeaderText(newText) {
+    //     const heroFirstText = this.querySelector('#heroFirstText');
+    //     if (heroFirstText) {
+    //         console.log(`Here we are`);
+    //         heroFirstText.innerHTML = newText;
+    //     }
+    // }
 }
 
 customElements.define('header-component', HeaderComponent);
